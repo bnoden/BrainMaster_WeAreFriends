b802e8ae896ede10031fffc1972e92a036b1ab10
bnoden <brandroid369@gmail.com>
2016-12-28 00:16:44 -0600

    bahaha

---
+++ Assets/_Script/Camera/Camera.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    public GameObject player;
+    Vector3 offset;
+
+    
+
+	// Use this for initialization
+	void Start () {
+	    offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	    MoveCam();
+	}
+    
+    void MoveCam() {
+
+        float Xmin = -0.6f;
+        float Xmax = 10.0f;
+        float Ymin = -1.0f;
+        float Ymax = 3.0f;
+        Vector3 playerPos = player.transform.position;
+        Vector3 camPos = transform.position;
+
+        camPos.x = Mathf.Clamp(playerPos.x, Xmin, Xmax);
+        camPos.y = Mathf.Clamp(playerPos.y, Ymin, Ymax);
+
+        transform.position = camPos;
+    }
+
+
+
+
+}
---
+++ Assets/_Script/Control/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 playerPos;
+    static float walk = 2.0f;
+    static float sprint = walk+2.0f;
+    static float step;
+
+	// Use this for initialization
+	void Start () {
+	    playerPos = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Move();
+	}
+
+    public void Attack() {
+        if (Input.GetButtonDown("Attack")) {
+            Debug.Log("SMAAAAAAAAAAAAASH!");
+        }
+    }
+
+    public void Move() {
+        Attack();
+        step = Time.deltaTime*walk;
+        if(Input.GetKey(KeyCode.LeftShift)) {
+            step = Time.deltaTime*sprint;
+        }
+        if(Input.GetKey(KeyCode.UpArrow)) {
+            playerPos.y+=step;
+        }
+        if(Input.GetKey(KeyCode.DownArrow)) {
+            playerPos.y-=step;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            playerPos.x-=step;
+        }
+        if(Input.GetKey(KeyCode.RightArrow)) {
+            playerPos.x+=step;
+        }
+        player.transform.position = playerPos;
+    }
+
+
+
+
+}
